285433818cb47fc364cc269a3c400f3d069f8683
istiaqhaider8 <istiaqhaider8@gmail.com>
2022-07-11 19:42:02 +0600

    Calculator using Asp.net

---
+++ Webapplication0ne/HelloworldUI.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HelloworldUI.aspx.cs" Inherits="Webapplication0ne.HelloworldUI" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        First Number
+                    </td>
+                    <td>
+                        <asp:TextBox runat="server" ID="firstNumberTextBox"></asp:TextBox>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        Second Number
+                    </td>
+                    <td>
+                        <asp:TextBox runat="server" ID="secondNumberTextBox"></asp:TextBox>
+                    </td>
+                </tr>
+
+                 <tr>
+                    <td>
+                        Result: 
+                    </td>
+                    <td>
+                        <asp:Label runat="server" ID="resultLabel"></asp:Label>
+                    </td>
+                </tr>
+
+                  <tr>
+                    <td>
+                      
+                    </td>
+                    <td>
+                        <asp:Button runat="server" ID="addButton" Text="Add" OnClick="addButton_Click"/>
+                        <asp:Button runat="server" ID="subtractButton" Text="Subtract" OnClick="subtractButton_Click"/>
+                        <asp:Button runat="server" ID="multiplyButton" Text="Multiply" OnClick="multiplyButton_Click"/>
+                        <asp:Button runat="server" ID="divisionButton" Text="Division" OnClick="divisionButton_Click"/>
+                    </td>
+                </tr>
+                
+            </table>
+           
+        </div>
+    </form>
+</body>
+</html>
---
+++ Webapplication0ne/HelloworldUI.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Webapplication0ne
+{
+    public partial class HelloworldUI : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Calculator");
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            double firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            double sceoundNumber = Convert.ToDouble(secondNumberTextBox.Text);
+            double result = Add(firstNumber, sceoundNumber);
+            resultLabel.Text = result.ToString();
+
+        }
+
+        protected void subtractButton_Click(object sender, EventArgs e)
+        {
+            double firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            double sceoundNumber = Convert.ToDouble(secondNumberTextBox.Text);
+            double result = subtract(firstNumber, sceoundNumber);
+            resultLabel.Text = result.ToString();
+
+        }
+
+        protected void multiplyButton_Click(object sender, EventArgs e)
+        {
+            double firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            double sceoundNumber = Convert.ToDouble(secondNumberTextBox.Text);
+            double result = multiply(firstNumber, sceoundNumber);
+            resultLabel.Text = result.ToString();
+
+        }
+
+        protected void divisionButton_Click(object sender, EventArgs e)
+        {
+            double firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            double sceoundNumber = Convert.ToDouble(secondNumberTextBox.Text);
+            double result = division(firstNumber, sceoundNumber);
+            resultLabel.Text = result.ToString();
+
+        }
+
+        private double Add(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        private double subtract(double firstNumber, double secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        private double multiply(double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        private double division(double firstNumber, double secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+
+    }
+}
